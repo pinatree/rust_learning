13fe14e97d465e21a58c34c0ff1727a06ca7711e
pinatree <pinatree.workmail@gmail.com>
2025-05-15 16:47:58 +0300

    simple functions

--- 1_hello_world/main.rs
+++ 1_hello_world/main.rs
@@ -1,3 +1,39 @@
+use std::{thread, time};
+
 fn main() {
     println!("Hello world from RUST!");
+    count_down();
+    fly_rocket();
+}
+
+fn count_down() {
+    let mut counter: u8;
+    let final_value: u8;
+    counter = 5;
+    final_value = 1;
+
+    loop {
+        let ten_millis = time::Duration::from_millis(250);
+        thread::sleep(ten_millis);
+
+        counter = counter - 1;
+        if counter < final_value {
+            break;
+        }
+        println!("Rocket start after { } seconds", counter);
+    }
+
+}
+
+fn fly_rocket() {    
+    for _ in 1..15 {
+        let output: &str;
+        output = "WZWZWZWWZWZWZWZWZ!";
+
+        println!("{}", output);
+
+        let ten_millis = time::Duration::from_millis(100);
+        thread::sleep(ten_millis);
+    }
+    println!("Rocket flew away!");
 }
